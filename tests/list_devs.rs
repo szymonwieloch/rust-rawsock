a3dcb1e72675742ff2b99472452527926f3c543f
szymonwieloch <szymon.wieloch@gmail.com>
2019-05-01 20:02:13 +0200

    Fixed test on Travis and prepared Windows env for Travis

--- tests/list_devs.rs
+++ tests/list_devs.rs
@@ -3,12 +3,20 @@ use rawsock::{PCap, Library,  InterfaceDescription};
 /*
 These tests assume:
 - You have the relevant library available in the operating system.
-- You have at least one network interface.
 */
 
 #[test]
 fn open_devices() {
     let pcap = PCap::open_default_locations().expect("Could not open pcap library");
     let devices: Vec<InterfaceDescription> = pcap.get_devices().expect("Could not get devices").collect();
-    assert!(! devices.is_empty());
+}
+
+#[test]
+#[ignore]
+fn find_devices(){
+    let pcap = PCap::open_default_locations().expect("Could not open pcap library");
+    let devices: Vec<InterfaceDescription> = pcap.get_devices().expect("Could not get devices").collect();
+    //funny fact: on travis Linux devices do not have any configured devices but MacOS have
+    //This is why this assertion cannot be checked by default
+    assert!(!devices.is_empty());
 }
