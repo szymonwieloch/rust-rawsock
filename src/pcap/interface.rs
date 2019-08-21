1ecc3ddb4e11f6abb8047cac3c289a03036a691c
spacemeowx2 <spacemeowx2@gmail.com>
2019-08-21 16:20:54 +0800

    use pcap_compile safely

--- src/pcap/interface.rs
+++ src/pcap/interface.rs
@@ -6,10 +6,15 @@ use super::structs::PCapStat;
 use libc::{ c_int};
 use std::mem::{uninitialized, transmute};
 use crate::utils::cstr_to_string;
+use std::sync::Mutex;
 
 use crate::pcap_common::helpers::{borrowed_packet_from_header, on_received_packet_static, on_received_packet_dynamic};
 use crate::pcap_common::constants::{SUCCESS, PCAP_ERROR_BREAK, PCAP_EMPTY_FILTER_STR};
 use crate::pcap_common::BpfProgram;
+
+lazy_static! {
+	static ref COMPILE_GUARD : Mutex<()> = Mutex::new(());
+}
 
 ///pcap version of interface.
 pub struct Interface<'a> {
@@ -119,7 +124,11 @@ impl<'a> traits::DynamicInterface<'a> for Interface<'a> {
 
     fn set_filter_cstr(&mut self, filter: &CStr) -> Result<(), Error> {
         let mut bpf_filter: BpfProgram = unsafe {uninitialized()};
-        let result = unsafe { self.dll.pcap_compile(self.handle, &mut bpf_filter, filter.as_ptr(), 1, 0) };
+        // before pcap 1.8, pcap_compile was not thread safe (https://www.tcpdump.org/manpages/pcap_compile.3pcap.html)
+        let result = {
+            let _lock = COMPILE_GUARD.lock().unwrap();
+            unsafe { self.dll.pcap_compile(self.handle, &mut bpf_filter, filter.as_ptr(), 1, 0) }
+        };
         if result != SUCCESS {
             return Err(self.last_error())
         }
