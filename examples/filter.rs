b123dd8d38adfb60012dd94df59e1cc75a16dcad
spacemeowx2 <spacemeowx2@gmail.com>
2019-08-21 17:12:50 +0800

    Updated filter example

--- examples/filter.rs
+++ examples/filter.rs
@@ -4,7 +4,7 @@
 
 extern crate rawsock;
 mod commons;
-use rawsock::{open_best_library, DataLink};
+use rawsock::open_best_library;
 
 fn main() {
     println!("Opening packet capturing library");
@@ -15,19 +15,14 @@ fn main() {
         .expect("There are no available interfaces").name.clone();
     println!("Opening the {} interface", &interf_name);
     let mut interf = lib.open_interface(&interf_name).expect("Could not open network interface");
-    if let DataLink::Ethernet = interf.data_link() {
-        println!("Interface opened, data link: {}", interf.data_link());
-    } else {
-        println!("data link should be Ethernet, {} found", interf.data_link());
-        return
-    }
+    println!("Interface opened, data link: {}", interf.data_link());
 
     interf.set_filter("icmp").expect("Could not set filter");
 
     //receive some packets.
-    println!("Receiving 5 packets:");
+    println!("Receiving 5 icmp packets:");
     for _ in 0..5 {
         let packet = interf.receive().expect("Could not receive packet");
-        println!("Received packet: {}", packet);
+        println!("Received icmp packet: {}", packet);
     }
 }
